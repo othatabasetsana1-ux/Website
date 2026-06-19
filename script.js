8c456d8c4fbda5284c999d1ea2ca0f51181d95b8
othatabasetsana1-ux <othatabasetsana1@gmail.com>
2026-06-19 19:46:52 +0200

    Refactor hamburger and navMenu functionality

--- script.js
+++ script.js
@@ -83,3 +83,14 @@ window.onclick = function(event) {
         }
     });
 };
+const hamburger = document.getElementById("hamburger");
+const navMenu = document.getElementById("navMenu");
+
+hamburger.addEventListener("click", () => {
+    navMenu.classList.toggle("active");
+});
+document.querySelectorAll(".nav-menu a").forEach(link => {
+    link.addEventListener("click", () => {
+        navMenu.classList.remove("active");
+    });
+});
